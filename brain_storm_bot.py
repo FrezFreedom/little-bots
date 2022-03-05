052c00c0ad686d2115d5e2e449eb1d8d30b44ebe
frez_freedom <frez1374@gmail.com>
2022-03-05 06:37:30 +0330

    Publish 0.01 version

--- brain_storm_bot.py
+++ brain_storm_bot.py
@@ -2,6 +2,12 @@ import requests
 import time
 from api_keys import *
 
+green_circle = u'\U0001F7E2'
+lollipop = u'\U0001F36D'
+doughnut = u'\U0001F369'
+rocket = u'\U0001F680'
+construction_sign = u'\U0001F6A7'
+
 
 def send_message_to_brain_storm(message):
     try:
@@ -12,4 +18,15 @@ def send_message_to_brain_storm(message):
         time.sleep(60)
 
 
-send_message_to_brain_storm("hey")
+message = f'''
+{rocket} <b>BrainStorm Time</b>
+Hey man, Think about <b>ideas</b>.
+You are the <b>best</b> thinker in The global.
+So you can find <b>awesome</b> ideas.
+Please send at least an idea till <b>12</b> hours later.
+Thanks!
+'''
+
+while True:
+    send_message_to_brain_storm(message)
+    time.sleep(12 * 60 * 60)
